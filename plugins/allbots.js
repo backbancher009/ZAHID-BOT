939529efa4ec2371f8584dd84b5d03249b264d7f
backbancher009 <jsjahid20249@gmail.com>
2026-04-10 08:43:27 +0600

    Update allbots.js

--- plugins/allbots.js
+++ plugins/allbots.js
@@ -18,22 +18,8 @@ module.exports = {
     const myNumber = "8801615298449";
     const msg = `🌟 *MY BOT COLLECTION* 🌟
 
-📱 *WhatsApp Bot*
-Link: https://github.com/MOHAMMAD-NAYAN-OFFICIAL/NAYAN-WHATSAPP-BOT
-Tutorial: https://youtu.be/hrO_v3z-Lq8
-
-💬 *Messenger Bot*
-Link: https://github.com/MOHAMMAD-NAYAN-OFFICIAL/Nayan-Bot
-Tutorial: https://youtu.be/DrQw3j56Llk
-
-🤖 *Telegram Bot*
-Link: https://github.com/MOHAMMAD-NAYAN-OFFICIAL/NAYAN-TELEGRAM-BOT
-Tutorial: https://youtu.be/FyjUL6MwaXs
-
 🛠️ *Support Channel*
-Link: https://chat.whatsapp.com/KuSent8x2rIIl93CD1aKBl?mode=ems_copy_c
-
-📞 *Contact Me*: +${myNumber}`;
+Link: https://chat.whatsapp.com/KuSent8x2rIIl93CD1aKBl?mode=ems_copy_c;
 
     await api.sendMessage(threadId, { text: msg }, { quoted: message });
   }
